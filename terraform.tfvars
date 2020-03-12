ae45c8a98e07bca5f0548102be379591c1a60e1a
alin <alin@localhost.localdomain>
2020-03-12 23:24:33 +0000

    solved some mistakes

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,5 @@
 region = "eu-west-2"
 ami = {
-    eu-west-2 = ami-050b8344d77081f4b
-    eu-west-1 = ami-0e61341fa75fcaa18
+  eu-west-2 = ami-050b8344d77081f4b
+  eu-west-1 = ami-0e61341fa75fcaa18
 }
--- web.tf
+++ web.tf
@@ -1,35 +1,35 @@
 provider "aws" {
-    region = var.region
+  region = var.region
 }
 
 module "vpc_basic" {
-    source = "git@github.com:AlinBBB/tf_vpc"
-    name = "web"
-    cidr = "10.0.0.0/16"
-    public_subnet = "10.0.1.0/24"
+  source        = "git@github.com:AlinBBB/tf_vpc"
+  name          = "web"
+  cidr          = "10.0.0.0/16"
+  public_subnet = "10.0.1.0/24"
 }
 
 resource "aws_instance" "web" {
-    ami   = var.ami[var.region]
-    instance_type = var.instance_type
-    key_name = var.key_name
-    subnet_id = module.vpc_basic.public_subnet_id
-    private_ip = var.instance_ips[count.index]
-    associate_public_ip_address = true
-    user_data = files("files/web_bootstrap.sh")
+  ami                         = var.ami[var.region]
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  subnet_id                   = module.vpc_basic.public_subnet_id
+  private_ip                  = var.instance_ips[count.index]
+  associate_public_ip_address = true
+  user_data                   = files("files/web_bootstrap.sh")
 
-    vpc_security_group_ids = [aws_security_group.web_host_sg.id,]
+  vpc_security_group_ids = [aws_security_group.web_host_sg.id, ]
 
-    tags = {
-        Name = "web-${format("%03d", count.index +1)}"
-        Owner = element(var.owner_tag,count.index)
-    }    
-    count = length(var.instance_ips)
+  tags = {
+    Name  = "web-${format("%03d", count.index + 1)}"
+    Owner = element(var.owner_tag, count.index)
+  }
+  count = length(var.instance_ips)
 
 }
 
 resource "aws_elb" "web" {
-  name = "web-elb"
+  name            = "web-elb"
   subnets         = [module.vpc_basic.public_subnet_id]
   security_groups = [aws_security_group.web_inbound_sg.id]
 
@@ -85,9 +85,9 @@ resource "aws_security_group" "web_host_sg" {
 
   # HTTP access from the VPC
   ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = [module.vpc_basic.cidr]
   }
 
