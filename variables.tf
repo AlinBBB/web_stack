395da1de6061018784bae2f7631ad0c3a0bffa93
alin <alin@localhost.localdomain>
2020-03-13 00:36:57 +0000

    eliminat cheia for now

--- variables.tf
+++ variables.tf
@@ -3,10 +3,10 @@ variable "region" {
   description = "The AWS region"
 }
 
-variable "key_name" {
+/*variable "key_name" {
   type        = string
   description = "The AWS key pair to use for resources"
-}
+}*/
 
 variable "ami" {
   type        = map(string)
--- web.tf
+++ web.tf
@@ -12,7 +12,7 @@ module "vpc_basic" {
 resource "aws_instance" "web" {
   ami                         = var.ami[var.region]
   instance_type               = var.instance_type
-  key_name                    = var.key_name
+  #key_name                    = var.key_name
   subnet_id                   = module.vpc_basic.public_subnet_id
   private_ip                  = var.instance_ips[count.index]
   associate_public_ip_address = true
